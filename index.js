b0f9ac4ac3767bed6a938396029b2268e69592b6
anissa guembre <guembreani@gmail.com>
2021-05-01 22:06:44 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName(){
+    customerName = "BOB"
+    return customerName
+}
+function setBestCustomer(){
+    bestCustomer = "not bob"
+    return bestCustomer
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+    return bestCustomer
+}
+const leastFavoriteCustomer = "Anna";
+function changeLeastFavoriteCustomer(){
+     leastFavoriteCustomer = "bibi";
+    return leastFavoriteCustomer
+
+}
